1b067006d104e6786234b86f1c2d90d73b257e8a
Eamonn de Leastar <edeleastar@wit.ie>
2023-03-19 18:00:06 +0000

    introduce +layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<a href="/about">About</a>
+<br />
+<a href="/dashboard">Dasbboard</a>
+<br />
+<a href="/settings">Settings</a>
+<hr />
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,1 @@
-<a href="/about">About</a>
-<br />
-<a href="/dashboard">Dasbboard</a>
-<br />
-<a href="/settings">Settings</a>
-<hr />
 <h1>Hello and welcome to my site!</h1>
