3a30a29ba2eb785d86b8c9b8688c8b698bc20765
Eamonn de Leastar <edeleastar@wit.ie>
2023-03-19 18:31:16 +0000

    introduce Nav component

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,6 @@
+<a href="/about">About</a>
+<br />
+<a href="/dashboard">Dasbboard</a>
+<br />
+<a href="/settings">Settings</a>
+<hr />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,6 @@
-<a href="/about">About</a>
-<br />
-<a href="/dashboard">Dasbboard</a>
-<br />
-<a href="/settings">Settings</a>
-<hr />
+<script>
+	import Nav from "$lib/Nav.svelte";
+</script>
 
+<Nav />
 <slot />
